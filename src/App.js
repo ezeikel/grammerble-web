52a11a89c466e86fa3c752764d481d4f5c52d2dd
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-11-17 10:13:58 -0800

    Small setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+//import Header from './components/Header';
+// import Nav from './components/Nav';
+import Main from './components/Main';
+//import './globalStyles';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
     );
   }
 }
